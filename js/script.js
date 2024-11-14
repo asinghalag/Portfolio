289e6d6c7cca55d7a26109d81a04a2a736f404fa
asinghalag <asinghalag@gmail.com>
2024-11-14 17:34:22 -0500

    Customized portfolio content based on resume

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if (document.getElementById('my-work-link')) {
+  document.getElementById('my-work-link').addEventListener('click', () => {
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('contact_button')) {
+  document.getElementById('contact_button').addEventListener('click', () => {
+    document.getElementById('footer').scrollIntoView({behavior: "smooth"})
+  })
+}
